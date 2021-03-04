a336db8b1bf7abda4684da8b72331687ee007098
TS22082 <ts22082@gmail.com>
2021-03-04 15:48:31 -0800

    remove unused env

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
-require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 // setup express
