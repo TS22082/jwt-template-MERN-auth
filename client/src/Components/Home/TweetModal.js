f201c34b1230397e631f2af97cf3587d3a72a72a
andrewjspivey <12spivey@gmail.com>
2022-01-21 16:48:01 -0800

    add post and list posts added

--- client/src/Components/Home/TweetModal.js
+++ client/src/Components/Home/TweetModal.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import UserContext from "../../Context/UserContext";
 import TweetModalContainer from "../../Fragments/Home/TweetModalContainer";
+import Axios from "axios";
+import AuthButton from "../../Fragments/Buttons/AuthButton";
 
 const TweetModal = (props) => {
+  const [tweet, setTweet] = useState("");
+  const { userData } = useContext(UserContext);
+
+  const onChange = (e) => {
+    setTweet(e.target.value);
+  };
+
+  const onSubmitTweet = async (e) => {
+    e.preventDefault();
+    try {
+      const headers = {
+        "x-auth-token": userData.token,
+      };
+      await Axios.post(
+        "/posts/",
+        {
+          authorId: userData.user.id,
+          text: tweet,
+        },
+        { headers: headers }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+    setTweet("");
+    props.setShow(false);
+  };
+
   return (
     <TweetModalContainer show={props.show}>
       <h3>What's Happening?</h3>
-      <form action="">
-        <input type="text" />
+      <form onSubmit={(e) => onSubmitTweet(e)}>
+        <input type="text" value={tweet} onChange={onChange} />
+        <AuthButton type="submit">Submit</AuthButton>
       </form>
     </TweetModalContainer>
   );
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -5,11 +5,24 @@ import HomeLayout from "../Fragments/Home/HomeLayout";
 import Aside from "../Components/Home/Aside";
 import FeedContainer from "../Fragments/Home/FeedContainer";
 import TweetModal from "../Components/Home/TweetModal";
+import Axios from "axios";
 
 const Home = () => {
+  const [tweets, setTweets] = useState([]);
   const [postModalShow, setPostModalShow] = useState(false);
   const { userData } = useContext(UserContext);
   const history = useHistory();
+
+  const getPosts = async () => {
+    const posts = await Axios.get("/posts/all", {
+      headers: { "x-auth-token": userData.token },
+    });
+    setTweets(posts.data);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, [tweets]);
 
   const openPostModal = () => {
     setPostModalShow(true);
@@ -25,7 +38,16 @@ const Home = () => {
 
       <FeedContainer>
         <h1>Twitter Feed</h1>
-        <TweetModal show={postModalShow}></TweetModal>
+        <TweetModal
+          show={postModalShow}
+          setShow={setPostModalShow}
+        ></TweetModal>
+
+        {tweets.map((tweet, i) => (
+          <div key={i}>
+            <h3>{tweet.text}</h3>
+          </div>
+        ))}
       </FeedContainer>
       <div>Whats happening/Messages</div>
     </HomeLayout>
