f769b798b31c0349c694a42dc322716088321292
TS22082 <ts22082@gmail.com>
2022-01-23 11:19:47 -0800

    fix(return):return re.send so multiple responses dont happen

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,16 +7,9 @@ const {
   remove,
 } = require("../controllers/UserController");
 
-// register an account
 router.post("/register", register);
-
-// login user
 router.post("/login", login);
-
-// delete a user
 router.delete("/delete", auth, remove);
-
-// get current user
 router.get("/", auth, getUser);
 
 module.exports = router;
